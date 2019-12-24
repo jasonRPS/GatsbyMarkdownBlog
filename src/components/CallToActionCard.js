e5a92c47b43f97e95d8b480c9d806ea285b8284d
Jason Sorenson <jason.sorenson@RPSCorporation.local>
2019-12-24 13:15:22 -0600

    stuff

--- src/components/CallToActionCard.js
+++ src/components/CallToActionCard.js
@@ -3,16 +3,38 @@ import styled from "styled-components"
 
 const CallToActionCardStyles = styled.div`
   #container {
-    width: 300px;
-    height: 300px;
-    border: 2px solid black;
+    width: 280px;
+    margin: 1rem;
+    h1 {
+      font-size: 1rem;
+      font-weight: normal;
+      font-style: italic;
+    }
+
+    #image-container {
+      width: 280px;
+      height: 280px;
+      border: 2px solid black;
+
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
   }
 `
 
-const CallToActionCard = () => {
+const CallToActionCard = props => {
   return (
     <CallToActionCardStyles>
-      <div id="container"></div>
+      <div id="container">
+        <h1>{props.title}</h1>
+        <div id="image-container">
+          <img src={props.image} />
+        </div>
+        <p>{props.callToAction}</p>
+      </div>
     </CallToActionCardStyles>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,10 +15,26 @@ const IndexPage = () => {
           <Head title="Home" />
           <div id="hero-image">Hero Image and Hero Story</div>
           <div id="call-to-action">
-            <CallToActionCard />
-            <CallToActionCard />
-            <CallToActionCard />
-            <CallToActionCard />
+            <CallToActionCard
+              title="Clean great stuff"
+              callToAction="Integrate your tools and teams for deeper insights and faster threat response"
+              image="https://source.unsplash.com/random/1920x1080"
+            />
+            <CallToActionCard
+              title="Clean great stuff"
+              callToAction="Integrate your tools and teams for deeper insights and faster threat response"
+              image="https://source.unsplash.com/random/1920x1080"
+            />
+            <CallToActionCard
+              title="Clean great stuff"
+              callToAction="Integrate your tools and teams for deeper insights and faster threat response"
+              image="https://source.unsplash.com/random/1920x1080"
+            />
+            <CallToActionCard
+              title="Clean great stuff"
+              callToAction="Integrate your tools and teams for deeper insights and faster threat response"
+              image="https://source.unsplash.com/random/1920x1080"
+            />
           </div>
           <h1>Hello</h1>
           <h2>factory Cat</h2>
