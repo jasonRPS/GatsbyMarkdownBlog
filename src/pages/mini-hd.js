4fa389cc25635f3b1f807e0a176214dd771b3817
Jason Sorenson <jason.sorenson@RPSCorporation.local>
2020-02-18 15:17:48 -0600

    working on secificatoin nav

--- src/pages/mini-hd.js
+++ src/pages/mini-hd.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/Layout"
 import ProductInfo from "../components/ProductInfo"
 import styled from "styled-components"
+import SeeMoreButton from "../components/controls/SeeMoreButton"
 
 const MiniHDPageStyles = styled.div`
   #product-container {
@@ -9,6 +10,12 @@ const MiniHDPageStyles = styled.div`
     margin-top: 100px;
     margin-right: auto;
     margin-left: auto;
+  }
+
+  #spec-nav {
+    width: 100%;
+    height: 50px;
+    background-color: #2d2d2d;
   }
 `
 
@@ -18,6 +25,12 @@ const MiniHDPage = () => {
       <MiniHDPageStyles>
         <div id="product-container">
           <ProductInfo />
+        </div>
+        <div id="spec-nav">
+          <ul>
+            <li>Options</li>
+            <li>Specs</li>
+          </ul>
         </div>
       </MiniHDPageStyles>
     </Layout>
