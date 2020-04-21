892e42c90d6d5d924ae4692ef29000649c09a3e2
Jason Sorenson <jason.sorenson@RPSCorporation.local>
2020-04-21 14:01:18 -0500

    work on new slider

---
+++ src/components/slider/Slider.js
@@ -0,0 +1,105 @@
+import React from "react"
+import { SliderCss } from "./styles/SliderCss"
+import SlideImage1 from "../../images/fc-image.jpg"
+import SlideImage2 from "../../images/fc-image2.jpg"
+import SlideImage3 from "../../images/fc-image3.jpg"
+import SlideImage4 from "../../images/fc-image4.jpg"
+import SlideImage5 from "../../images/fc-image5.jpg"
+
+const Slider = () => {
+  return (
+    <SliderCss>
+      <div class="gallery">
+        <div className="slide-container" id="slide-1">
+          <img src={SlideImage1} alt="" />
+          <div className="slide-info" id="slide-info1">
+            <h2>Industrial Floor Scrubbers</h2>
+            <p>
+              Factory Cat is known World Wide for their industrial strength and
+              quality design when it comes to Walk Behind Scrubber Dryers.
+            </p>
+            <button>Learn More</button>
+          </div>
+        </div>
+
+        <div className="slide-container" id="slide-2">
+          <img src={SlideImage2} alt="" />
+          <div className="slide-info" id="slide-info2">
+            <h2>Industrial Floor Scrubbers</h2>
+            <p>
+              Factory Cat is known World Wide for their industrial strength and
+              quality design when it comes to Walk Behind Scrubber Dryers.
+            </p>
+            <button>Learn More</button>
+          </div>
+        </div>
+
+        <div className="slide-container" id="slide-3">
+          <img src={SlideImage3} alt="" />
+          <div className="slide-info" id="slide-info2">
+            <h2>Industrial Floor Scrubbers</h2>
+            <p>
+              Factory Cat is known World Wide for their industrial strength and
+              quality design when it comes to Walk Behind Scrubber Dryers.
+            </p>
+            <button>Learn More</button>
+          </div>
+        </div>
+
+        <div className="slide-container" id="slide-4">
+          <img src={SlideImage4} alt="" />
+          <div className="slide-info" id="slide-info2">
+            <h2>Industrial Floor Scrubbers</h2>
+            <p>
+              Factory Cat is known World Wide for their industrial strength and
+              quality design when it comes to Walk Behind Scrubber Dryers.
+            </p>
+            <button>Learn More</button>
+          </div>
+        </div>
+
+        <div className="slide-container" id="slide-5">
+          <img src={SlideImage5} alt="" />
+          <div className="slide-info" id="slide-info2">
+            <h2>Industrial Floor Scrubbers</h2>
+            <p>
+              Factory Cat is known World Wide for their industrial strength and
+              quality design when it comes to Walk Behind Scrubber Dryers.
+            </p>
+            <button>Learn More</button>
+          </div>
+        </div>
+      </div>
+
+      <div id="slider-nav">
+        <a href="#slide-1">
+          <div className="nav-elm">
+            <p>Here is Slide 1 And something else if we need more</p>
+          </div>
+        </a>
+        <a href="#slide-2">
+          <div className="nav-elm">
+            <p>Here is Slide 2</p>
+          </div>
+        </a>
+        <a href="#slide-3">
+          <div className="nav-elm">
+            <p>Here is Slide 3</p>
+          </div>
+        </a>
+        <a href="#slide-4">
+          <div className="nav-elm">
+            <p>Here is Slide 4</p>
+          </div>
+        </a>
+        <a href="#slide-5">
+          <div className="nav-elm">
+            <p>Here is Slide 5</p>
+          </div>
+        </a>
+      </div>
+    </SliderCss>
+  )
+}
+
+export default Slider
---
+++ src/components/slider/styles/SliderCss.js
@@ -0,0 +1,72 @@
+import styled from "styled-components"
+
+export const SliderCss = styled.div`
+  .gallery {
+    display: flex;
+    /* height: 70vh; */
+    overflow: hidden;
+    scroll-snap-type: y mandatory;
+    scroll-behavior: smooth;
+  }
+
+  .slide-container {
+    position: relative;
+    width: 100vw;
+    height: 70vh;
+
+    img {
+      width: 100vw;
+      height: 100%;
+      object-fit: cover;
+    }
+    .slide-info {
+      position: absolute;
+      background-color: rgba(0, 0, 0, 0.7);
+      padding: 1.5rem;
+      max-width: 450px;
+      text-align: center;
+      color: white;
+      top: 100px;
+      right: 200px;
+
+      button {
+        border: none;
+        background-color: #ed2024;
+        font-size: 1em;
+        color: white;
+        padding: 0.5rem 1rem;
+      }
+    }
+  }
+
+  #slider-nav {
+    display: flex;
+    position: absolute;
+    left: 50%;
+    margin-left: -495px;
+    bottom: 250px;
+    z-index: 2;
+
+    .nav-elm {
+      width: 150px;
+      text-align: center;
+      text-decoration: none;
+      margin: auto;
+      width: 178px;
+      height: 90px;
+      display: table;
+      border: 1px solid black;
+      padding: 0 10px;
+      color: white;
+
+      background-color: rgba(0, 0, 0, 0.8);
+      &:hover {
+        background-color: rgba(237, 32, 36, 0.7);
+      }
+    }
+
+    a {
+      text-decoration: none;
+    }
+  }
+`
