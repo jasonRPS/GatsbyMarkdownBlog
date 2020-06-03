287469dd95db5e606653a0473dd69d2a32c35432
Jason Sorenson <jason.sorenson@RPSCorporation.local>
2020-06-03 12:38:57 -0500

    checking nav

--- src/components/navigation/MobileNavigation.js
+++ src/components/navigation/MobileNavigation.js
@@ -10,12 +10,64 @@ const MobileNavStyles = styled.div`
   border-bottom-style: double;
   border-color: #ed2024;
   border-width: 10px;
+  border-bottom-right-radius: 90px;
+  color: white;
+  font-size: 1.5em;
+
+  ul {
+    margin: 0;
+    list-style-type: none;
+    border-bottom: 1px solid #ed2024;
+
+    li {
+      padding: 1rem;
+    }
+  }
+
+  select {
+    padding: 0;
+    font-size: 1em;
+    border: none;
+    background: transparent;
+    color: white;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    text-indent: 1px;
+    text-overflow: "";
+  }
+
+  option {
+  }
 `
 
 const MobileNavigation = () => {
   return (
     <MobileNavStyles>
-      Here is my mobile navigation. it is awesome
+      <ul>
+        <li>Home</li>
+        <li>
+          <select id="products">
+            <option disabled selected value="0">
+              Products
+            </option>
+            <option value="1">Walk behind scrubbers</option>
+            <option value="2">Walk behind sweepers</option>
+            <option value="3">Ride-on scrubbers</option>
+            <option value="4">Ride-on scrubbers</option>
+            <option value="5">Sweeper-Scrubber</option>
+          </select>
+        </li>
+        <li>
+          <select name="support">
+            <option disabled selected value="0">
+              Support
+            </option>
+            <option value="1">Manual Downloads</option>
+            <option value="2">Contact</option>
+          </select>
+        </li>
+        <li>Media</li>
+      </ul>
     </MobileNavStyles>
   )
 }
