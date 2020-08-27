bd17e9be736701bdefc118d142228a9c08b65ff9
Jason Sorenson <jason.sorenson@RPSCorporation.local>
2020-08-27 13:46:06 -0500

    testaroniy

--- src/components/testComponent.js
+++ src/components/testComponent.js
@@ -33,11 +33,11 @@ const testComponent = () => {
       <TestStyleSheet>
         <div id="rj-player">
           <div class="player-v3 player-medium">
-            {/* <div id="rj-cover">
+            <div id="rj-cover">
               <a href="#">
                 <img src="" />
               </a>
-            </div> */}
+            </div>
             <div class="info">
               <div class="rjp-trackinfo-container">
                 <h4 class="rjp-label">Now playing:</h4>
@@ -103,15 +103,74 @@ const testComponent = () => {
         </div>
       </TestStyleSheet>
       <StyledThing>
-        <div id="rj-cover">
-          <a href="#">
-            <img src="" />
-          </a>
-        </div>
-        <div class="info">
-          <div class="rjp-trackinfo-container">
-            <h4 class="rjp-label">Now playing:</h4>
-            <p id="trackInfo" class="rjp-info"></p>
+        <div id="rj-player">
+          <div class="player-v3 player-medium">
+            <div id="rj-cover">
+              <a href="#">
+                <img src="" />
+              </a>
+            </div>
+            <div class="info">
+              <div class="rjp-trackinfo-container">
+                <h4 class="rjp-label">Now playing:</h4>
+                <p id="trackInfo" class="rjp-info"></p>
+              </div>
+              <div class="rjp-player-container">
+                <div id="rjp-radiojar-player"></div>
+                <div id="rj-player-controls" class="rj-player-controls">
+                  <div class="jp-gui jp-interface">
+                    <div class="jp-controls">
+                      <a
+                        href="javascript:;"
+                        style={{ display: "block" }}
+                        class="jp-play"
+                        title="Play"
+                      >
+                        &nbsp;<i class="icon-play"></i>
+                      </a>
+                      <a
+                        href="javascript:;"
+                        style={{ display: "none" }}
+                        class="jp-pause"
+                        title="Pause"
+                      >
+                        <i class="icon-pause"></i>
+                      </a>
+                      <a
+                        href="javascript:;"
+                        style={{ display: "block" }}
+                        class="jp-mute"
+                        title="Mute"
+                      >
+                        <i class="icon-volume-up"></i>
+                      </a>
+                      <a
+                        href="javascript:;"
+                        style={{ display: "none" }}
+                        class="jp-unmute"
+                        title="Unmute"
+                      >
+                        <i class="icon-volume-off"></i>
+                      </a>
+                      <div class="jp-volume-bar-wrapper">
+                        <div class="jp-volume-bar">
+                          <div class="jp-volume-bar-value"></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="jp-no-solution">
+                  <span>Update Required</span>
+                  To play the media you will need to either update your browser
+                  to a recent version or update your{" "}
+                  <a href="//get.adobe.com/flashplayer/" target="_blank">
+                    Flash plugin
+                  </a>
+                  .
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <audio controls>
